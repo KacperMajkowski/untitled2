ff906d983dd68efab0159f8ba8bc188f7152e761
KacperMajkowski <kacper20015@o2.pl>
2021-10-15 09:14:40 +0200

    Ammend1

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import com.google.gson.Gson;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Book book = new Book("Tolkien", "Lord of the rings");
+
+        Gson gson = new Gson();
+        String bookJson = gson.toJson(book);
+
+        System.out.println(bookJson);
+
+    }
+}
